ffb5f4446ef14fd44a175773311115808c4ddf8c
Anam Rani <Anam Rani>
2022-11-08 06:23:21 +0500

    1st callback example

--- callback.js
+++ callback.js
@@ -1,1 +1,9 @@
 // Concurrency means doing tasks simultaneously
+function doFlexibleStuff(executeStuff){
+    executeStuff();
+    console.log("hello callback!");
+    
+}
+const function1 = () => console.log("HELLO FROM function");
+
+doFlexibleStuff(function1);
