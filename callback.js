985d0a19357dd9ba1373920f4e3a512c72d31108
Anam Rani <Anam Rani>
2022-11-08 06:42:31 +0500

    exapmle is set

--- callback.js
+++ callback.js
@@ -1,14 +1,14 @@
-// Concurrency means doing tasks simultaneously
-function doFlexibleStuff(executeStuff){
-    executeStuff();
-    console.log("hello callback!");
+// // Concurrency means doing tasks simultaneously
+// function doFlexibleStuff(executeStuff){
+//     executeStuff();
+//     console.log("hello callback!");
     
-}
-const function1 = () => console.log("HELLO FROM function");
+// }
+// const function1 = () => console.log("HELLO FROM function");
 
-doFlexibleStuff(function1);
+// doFlexibleStuff(function1);
 function doSomething(callback){
     callback();
 }
-const sayHi = () => "hi from callback fucntion";
+const sayHi = () => console.log("hi from callback fucntion");
 doSomething(sayHi);
