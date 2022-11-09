57f1d06920ab075d43b1762447abee3b05d60879
Anam Rani <Anam Rani>
2022-11-09 09:49:29 +0500

    practice ereicse is completed

--- callback.js
+++ callback.js
@@ -14,47 +14,61 @@
 // const sayHi = () => console.log("hi from callback fucntion");
 // doSomething(sayHi);
 
-//  Example # 02
+// //  Example # 02
 
-function judge(grade) {
-  switch (grade) {
-    case "A":
-      console.log(`you got an ${grade} :  amazing!`);
-      break;
-    case "B":
-      console.log(`you got a ${grade} : well done!`);
-      break;
-    case "C":
-      console.log(`you got a ${grade} : alright!`);
-      break;
-    case "D":
-      console.log(`you got a ${grade} : hmmm....`);
-      break;
-    default:
-      console.log("A", grade, "! What?!");
-      break;
-  }
+// function judge(grade) {
+//   switch (grade) {
+//     case "A":
+//       console.log(`you got an ${grade} :  amazing!`);
+//       break;
+//     case "B":
+//       console.log(`you got a ${grade} : well done!`);
+//       break;
+//     case "C":
+//       console.log(`you got a ${grade} : alright!`);
+//       break;
+//     case "D":
+//       console.log(`you got a ${grade} : hmmm....`);
+//       break;
+//     default:
+//       console.log("A", grade, "! What?!");
+//       break;
+//   }
+// }
+
+// function getGrade(score, callback) {
+//   let k
+//   grade;
+//   switch (true) {
+//     case score >= 90:
+//       grade = "A";
+//       break;
+//     case score >= 80:
+//       grade = "B";
+//       break;
+//     case score >= 70:
+//       grade = "C";
+//       break;
+//     case score >= 60:
+//       grade = "D";
+//       break;
+//     default:
+//       grade = "F";
+//       break;
+//   }
+//   callback(grade);
+// }
+// getGrade(85, judge);
+
+// // Practice exercise 13.1
+
+function greet(fullName) {
+  fullName.forEach(e => console.log(`hello doing welcome ${e}`));
 }
 
-function getGrade(score, callback) {
-  let grade;
-  switch (true) {
-    case score >= 90:
-      grade = "A";
-      break;
-    case score >= 80:
-      grade = "B";
-      break;
-    case score >= 70:
-      grade = "C";
-      break;
-    case score >= 60:
-      grade = "D";
-      break;
-    default:
-      grade = "F";
-      break;
-  }
-  callback(grade);
+function getGreeting(str, callback) {
+ const strArray = str.split("");
+ callback(strArray);
 }
-getGrade(85, judge);
+getGreeting("Anam" , greet);
+
